f9246dba14eb1a33fd6dff26b892fe3b14120d55
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2013-03-29 01:16:16 +0000

    Updated path to icon file.

--- src/pyqso.py
+++ src/pyqso.py
@@ -39,7 +39,7 @@ class PyQSO(Gtk.Window):
       self.set_size_request(500, 300)
       self.set_position(Gtk.WindowPosition.CENTER)
       try:
-         self.set_icon_from_file("../ico/log_64x64.png")
+         self.set_icon_from_file("../icons/log_64x64.png")
       except Exception, error:
          print error.message
 
