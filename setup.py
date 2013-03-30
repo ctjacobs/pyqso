74662a5d2859a00d70797b9bd2f29595570afda7
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2013-03-30 20:52:15 +0000

    Added a setup file for the pyqso package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyQSO',
+      version='Development',
+      description='A Python-based QSO logging tool',
+      author='Christian Jacobs',
+      author_email='',
+      url='https://launchpad.net/pyqso',
+      packages=['pyqso'],
+     )
+
