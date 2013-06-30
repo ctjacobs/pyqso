5d0132422d0899267b0276acf6fbe266ccba7913
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2013-06-30 23:04:37 +0100

    - Added the instantiation of the ConfigParser class on application startup. - Added some startup and qrz.com lookup options to the user preferences dialog.

--- pyqso/preferences_dialog.py
+++ pyqso/preferences_dialog.py
@@ -21,6 +21,9 @@
 from gi.repository import Gtk, GObject
 import logging
 
+import ConfigParser
+import os.path
+
 class PreferencesDialog(Gtk.Dialog):
    
    def __init__(self, root_window):
@@ -28,12 +31,19 @@ class PreferencesDialog(Gtk.Dialog):
 
       Gtk.Dialog.__init__(self, title="Preferences", parent=root_window, flags=Gtk.DialogFlags.DESTROY_WITH_PARENT, buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK))
 
+      # Get any application-specific preferences from the configuration file
+      config = ConfigParser.ConfigParser()
+      have_config = (config.read(os.path.expanduser('~/.pyqso.cfg')) != [])
+
       self.preferences = Gtk.Notebook()
 
-      self.visible_columns = Gtk.VBox()
-      self.preferences.insert_page(self.visible_columns, Gtk.Label("Visible Columns"), 0)
+      self.general = GeneralPage()
+      self.preferences.insert_page(self.general, Gtk.Label("General"), 0)
 
-      self.vbox.pack_start(self.preferences, True, True, 0)
+      self.view = Gtk.VBox()
+      self.preferences.insert_page(self.view, Gtk.Label("View"), 1)
+
+      self.vbox.pack_start(self.preferences, True, True, 2)
       self.show_all()
 
       return
@@ -41,3 +51,55 @@ class PreferencesDialog(Gtk.Dialog):
    def commit(self):
       # Commit the changes to the preferences.
       return
+
+class GeneralPage(Gtk.VBox):
+   
+   def __init__(self):
+      logging.debug("New GeneralPage instance created!")
+
+      Gtk.VBox.__init__(self, spacing=2)
+
+      self.sources = {}
+
+      frame = Gtk.Frame()
+      frame.set_label("Startup")
+      hbox = Gtk.HBox()
+      self.sources["SHOW_TOOLBOX"] = Gtk.CheckButton("Show toolbox by default")
+      self.sources["SHOW_TOOLBOX"].set_active(False)
+      hbox.pack_start(self.sources["SHOW_TOOLBOX"], False, False, 2)
+      frame.add(hbox)
+      self.pack_start(frame, True, True, 2)
+
+      frame = Gtk.Frame()
+      frame.set_label("Callsign lookup (using qrz.com)")
+      inner_vbox = Gtk.VBox()
+
+      hbox = Gtk.HBox()
+      hbox.pack_start(Gtk.Label("Username: "), False, False, 2)
+      self.sources["QRZ_USERNAME"] = Gtk.Entry()
+      hbox.pack_start(self.sources["QRZ_USERNAME"], False, False, 2)
+      inner_vbox.pack_start(hbox, True, True, 2)
+
+      hbox = Gtk.HBox()
+      hbox.pack_start(Gtk.Label("Password: "), False, False, 2)
+      self.sources["QRZ_PASSWORD"] = Gtk.Entry()
+      hbox.pack_start(self.sources["QRZ_PASSWORD"], False, False, 2)
+      inner_vbox.pack_start(hbox, True, True, 2)
+
+      frame.add(inner_vbox)
+      self.pack_start(frame, True, True, 2)
+
+      return
+
+class ViewPage(Gtk.VBox):
+   
+   def __init__(self):
+      logging.debug("New ViewPage instance created!")
+
+      Gtk.VBox.__init__(self, spacing=2)
+
+      self.label = Gtk.Label("Hello")
+      self.pack_start(self.label, True, True, 2)
+
+      return
+
